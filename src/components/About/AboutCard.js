5db294b32e7df28ce4fa9f28a40850845b925d70
Dharmik Batra <dharmikbatra@Dharmiks-MacBook-Air.local>
2026-03-21 18:00:14 +0530

    updated favicon

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
-import ytbImg from "../../Assets/youtube-icon.webp"
-import Button from "react-bootstrap/Button";
 
 
 function AboutCard() {
